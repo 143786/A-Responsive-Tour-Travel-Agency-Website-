52c3c33545607858b789d418e0d39e4a6c979e5d
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-07-24 20:56:45 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -9,6 +9,9 @@ let navbar = document.querySelector(".navbar");
 window.onscroll = () => {
   searchBtn.classList.remove("fa-times");
   searchBar.classList.remove("active");
+  menu.classList.remove("fa-times");
+  navbar.classList.remove("active");
+  loginForm.classList.remove("active");
 };
 menu.addEventListener("click", () => {
   menu.classList.toggle("fa-times");
